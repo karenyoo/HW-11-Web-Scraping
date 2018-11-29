c9d39d5369450459d4ef08b0977b923a013094fa
karenyoo <karenyooo@gmail.com>
2018-11-28 18:54:33 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "fJmqcTS43FrkLgHYC0anNPoSe"
+consumer_secret = "LWExBI8gxA29QfaRWXOfyK3iIZJLofAtv1vgXDAgjFOV3lBD4c"
+access_token = "1020112207701393408-YK0MPOp3JxYGTNO6jkHi2ZuiL19qUb"
+access_token_secret = "cGBSUPrM3pOhubEpGldrD1VwYiz4Bl0BYlsZBpfUviG9I"
